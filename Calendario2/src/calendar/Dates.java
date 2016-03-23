fc7ef30603d7f1dd05befdd20f6753c43a9218d0
Tiego <tiegoparotivo@gmail.com>
2016-03-23 10:42:02 -0300

    Arrumando os comentários

--- Calendario2/src/calendar/Dates.java
+++ Calendario2/src/calendar/Dates.java
@@ -12,13 +12,16 @@ public class Dates {
 		int ano = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira um ano"));
 
 		
-		if (ano < 1900 || ano > 2999 || mes < 1 || mes > 12){ // Verifica se a data é válida
+		// Verifica se a data é válida
+		if (ano < 1900 || ano > 2999 || mes < 1 || mes > 12){ 
 			System.out.println("Data inválida");
 		} 
 			
+		
+		// Verificar se o mês de fevereiro tem 28 ou 29 dias e se os meses de Abril, Junho, Setembro e Novembro possuem 30 dias.
 		else if ((dia <= 31) && (mes <= 12)){ 
 				
-			if (((dia > 28) && (mes == 2)) && (ano % 4 != 0) || ((dia == 31) && ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)))){ // Verificar se o mês de fevereiro tem 28 ou 29 dias e se os meses de Abril, Junho, Setembro e Novembro possuem 30 dias.   
+			if (((dia > 28) && (mes == 2)) && (ano % 4 != 0) || ((dia == 31) && ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)))){    
 				System.out.println("Data inválida");
 			}
 				
